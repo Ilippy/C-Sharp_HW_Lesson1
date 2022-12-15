0eb220e6067032e8f702274addb7719997ae7c79
Ilippy <Ilippyk@gmail.com>
2022-12-15 15:48:50 +0300

    add Task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = EnterNumber("Введите число не равное 0");
+        int increment;
+        if (number > 0)
+        {
+            increment = 1;
+        }
+        else
+        {
+            increment = -1;
+        }
+        System.Console.Write($"{number} -> ");
+        for (int i = number * -1; i != number + increment; i += increment)
+        {
+            if (i != number)
+            {
+                System.Console.Write($"{i}, ");
+            }
+            else
+            {
+                System.Console.Write(i);
+            }
+        }
+
+
+    }
+
+
+    static int EnterNumber(string str)
+    {
+        System.Console.WriteLine(str);
+        string stringNumber = Console.ReadLine().Trim();
+        int number;
+        while (!int.TryParse(stringNumber, out number) || number == 0)
+        {
+            Console.WriteLine("Ошибка! Введите одно целое число не равное 0");
+            stringNumber = Console.ReadLine().Trim();
+        }
+        return number;
+    }
+}
