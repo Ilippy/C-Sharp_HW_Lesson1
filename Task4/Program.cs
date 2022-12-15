7c979e6ad2e755c7db74a3f93adaadab31385722
Ilippy <Ilippyk@gmail.com>
2022-12-15 13:53:21 +0300

    add Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number1 = EnterNumber("Введите первое число");
+        int number2 = EnterNumber("Введите второе число");
+        int number3 = EnterNumber("Введите третье число");
+
+        int max = number1;
+
+        if(number2 > max) max = number2;
+        if(number3 > max) max = number3;
+
+        System.Console.WriteLine($"Из чисел {number1} {number2} {number3} самое большее число {max}");
+        
+    }
+
+
+    static int EnterNumber(string str)
+    {
+        System.Console.WriteLine(str);
+        string stringNumber = Console.ReadLine().Trim();
+        int number;
+        while (!int.TryParse(stringNumber, out number))
+        {
+            Console.WriteLine("Ошибка! Введите одно целое число");
+            stringNumber = Console.ReadLine().Trim();
+        }
+        return number;
+    }
+}
